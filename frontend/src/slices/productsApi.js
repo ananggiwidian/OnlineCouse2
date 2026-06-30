69be517709912adec7ffab0c236ba81ddc9281fc
Ananggi Widian <ananggiwidian@gmail.com>
2026-06-30 12:34:39 +0700

    fix: update API path to /api/products

--- frontend/src/slices/productsApi.js
+++ frontend/src/slices/productsApi.js
@@ -1,19 +1,17 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({ 
-    baseUrl: "" // <-- KOSONGKAN! (sebelumnya "http://localhost:5000/")
+    baseUrl: "" // tetap kosong
   }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
-      query: () => `/products`, // <-- TAMBAHKAN SLASH di depan (sebelumnya "products")
+      query: () => `/api/products`, // <-- TAMBAHKAN /api/
     }),
   }),
 });
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
+
 export const { useGetAllProductsQuery } = productsApi;
